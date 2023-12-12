76dbd5ff4402e6dd48b082843c56034b3d62a960
Diego Gonzales <diegoegj14@gmail.com>
2023-12-12 11:56:38 -0500

    Add calculator class and unit tests

---
+++ projects/01-testing-services/src/app/calculator.spec.ts
@@ -0,0 +1,39 @@
+import { Calculator } from './calculator';
+
+describe('Calculator', () => {
+  it('#multiply method should return 9', () => {
+    // Arrange
+    const calculator = new Calculator();
+    // Act
+    const result = calculator.multiply(3, 3);
+    // Assert
+    expect(result).toBe(9);
+  });
+
+  it('#divide method should return some numbers', () => {
+    const calculator = new Calculator();
+    const result1 = calculator.divide(6, 3);
+    expect(result1).toEqual(2);
+
+    const result2 = calculator.divide(5, 2);
+    expect(result2).toEqual(2.5);
+  });
+
+  it('testing matchers', () => {
+    const name1 = 'Diego';
+    let name2;
+
+    expect(name1).toBeDefined();
+    expect(name2).toBeUndefined();
+
+    expect(1 + 3 === 4).toBeTruthy();
+    expect(1 + 1 === 3).toBeFalsy();
+
+    expect(5).toBeLessThan(10);
+    expect(20).toBeGreaterThan(10);
+
+    expect('123456').toMatch(/123/);
+
+    expect(['apples', 'oranges', 'pears']).toContain('oranges');
+  });
+});
