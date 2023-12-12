af51e08fa5566a79cf51217029631f06fbe0c709
Diego Gonzales <diegoegj14@gmail.com>
2023-12-12 11:56:22 -0500

    Add calculator functionality to AppComponent

--- projects/01-testing-services/src/app/app.component.ts
+++ projects/01-testing-services/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '01-testing-services';
+
+  ngOnInit(): void {
+    const calculator = new Calculator();
+
+    const result1 = calculator.multiply(3, 3);
+    console.log(result1);
+
+    const result2 = calculator.divide(3, 0);
+    console.log(result2);
+  }
 }
