6a6743a6a430f2255ae27c61059ee16d1e7ace26
Diego Gonzales <diegoegj14@gmail.com>
2023-12-13 21:09:44 -0500

    Add new components and styles

---
+++ projects/01-testing-services/src/app/components/milligram-css/milligram-css.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MilligramCssComponent } from './milligram-css.component';
+
+describe('MilligramCssComponent', () => {
+  let component: MilligramCssComponent;
+  let fixture: ComponentFixture<MilligramCssComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MilligramCssComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MilligramCssComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/01-testing-services/src/app/components/milligram-css/milligram-css.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-milligram-css',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './milligram-css.component.html',
+  styleUrl: './milligram-css.component.css',
+})
+export default class MilligramCssComponent {}
