f4ffc950206f12d9dcc5c0ed0dad3d736906dd08
Diego Gonzales <diegoegj14@gmail.com>
2023-12-14 15:45:48 -0500

    Refactor ProductsService tests and add new tests for getAll method

--- projects/01-testing-services/src/app/services/products.service.spec.ts
+++ projects/01-testing-services/src/app/services/products.service.spec.ts
@@ -7,7 +7,10 @@ import {
 import { ProductsService } from './products.service';
 import { Product } from '@models/products.interface';
 import { environment } from '../../environments/environment';
-import { generateManyProducts } from '../mocks/product.mock';
+import {
+  generateManyProducts,
+  generateOneProduct,
+} from '../mocks/product.mock';
 
 fdescribe('ProductsService', () => {
   let productsService: ProductsService;
@@ -36,10 +39,53 @@ fdescribe('ProductsService', () => {
       productsService.getAllSimple().subscribe((resp) => {
         // Assert
         expect(resp.length).toEqual(mockData.length);
+        expect(resp).toEqual(mockData);
         doneFn();
       });
 
       // HTTP config
+      const requestedUrl = `${environment.API_URL}/products`;
+      const req = httpTestingController.expectOne(requestedUrl);
+      req.flush(mockData);
+
+      httpTestingController.verify();
+    });
+  });
+
+  describe('Tests for "getAll" method', () => {
+    it('should return a product list', (doneFn) => {
+      const mockData: Product[] = generateManyProducts(10);
+
+      productsService.getAll().subscribe((resp) => {
+        expect(resp.length).toEqual(mockData.length);
+        doneFn();
+      });
+
+      const requestedUrl = `${environment.API_URL}/products`;
+      const req = httpTestingController.expectOne(requestedUrl);
+      req.flush(mockData);
+
+      httpTestingController.verify();
+    });
+
+    it('should return a product list with taxes', (doneFn) => {
+      const mockData: Product[] = [
+        {
+          ...generateOneProduct(),
+          price: 100, // 100 * .19 = 19
+        },
+        {
+          ...generateOneProduct(),
+          price: 200, // 200 * .19 = 38
+        },
+      ];
+
+      productsService.getAll().subscribe((resp) => {
+        expect(resp[0].taxes).toEqual(19);
+        expect(resp[1].taxes).toEqual(38);
+        doneFn();
+      });
+
       const requestedUrl = `${environment.API_URL}/products`;
       const req = httpTestingController.expectOne(requestedUrl);
       req.flush(mockData);
