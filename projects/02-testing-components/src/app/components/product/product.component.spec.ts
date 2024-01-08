bb07f1d89600b5fa2f87f385d84e46652531a52a
Diego Gonzales <diegoegj14@gmail.com>
2024-01-08 17:52:22 -0500

    Fix test cases and add product mock data

--- projects/02-testing-components/src/app/components/product/product.component.spec.ts
+++ projects/02-testing-components/src/app/components/product/product.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
+import { NgOptimizedImage } from '@angular/common';
+import { generateOneProduct } from '@mocks/product.mock';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
@@ -8,12 +10,14 @@ describe('ProductComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductComponent]
-    })
-    .compileComponents();
-    
+      imports: [ProductComponent, NgOptimizedImage],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+
+    component.product = generateOneProduct();
+
     fixture.detectChanges();
   });
 
