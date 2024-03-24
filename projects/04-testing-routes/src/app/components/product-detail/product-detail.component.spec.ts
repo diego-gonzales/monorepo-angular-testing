ff1485c792d173bef5a17f066a2344ce2536af37
Diego Gonzales <diegoegj14@gmail.com>
2024-03-24 18:04:05 -0500

    testing render of product detail

--- projects/04-testing-routes/src/app/components/product-detail/product-detail.component.spec.ts
+++ projects/04-testing-routes/src/app/components/product-detail/product-detail.component.spec.ts
@@ -2,12 +2,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import ProductDetailComponent from './product-detail.component';
 import { ActivatedRoute } from '@angular/router';
-import { ActivatedRouteStub, observableMock } from '../../../testing';
+import {
+  ActivatedRouteStub,
+  getTextContent,
+  observableMock,
+  queryElementByTestId,
+} from '../../../testing';
 import { ProductsService } from '@services/products.service';
 import { Location } from '@angular/common';
 import { generateOneProduct } from '@mocks/product.mock';
 
-fdescribe('ProductDetailComponent', () => {
+fdescribe('@ProductDetailComponent', () => {
   let component: ProductDetailComponent;
   let fixture: ComponentFixture<ProductDetailComponent>;
   let activatedRouteStub: ActivatedRouteStub;
@@ -40,20 +45,48 @@ fdescribe('ProductDetailComponent', () => {
       ProductsService,
     ) as jasmine.SpyObj<ProductsService>;
     locationSpy = TestBed.inject(Location) as jasmine.SpyObj<Location>;
+  });
 
+  it('#should create', () => {
     const productId = '1';
     const productMock = {
       ...generateOneProduct(),
       id: productId,
     };
 
-    activatedRouteStub.setParamMap({ id: '1' });
+    activatedRouteStub.setParamMap({ id: productId });
     productServiceSpy.getOne.and.returnValue(observableMock(productMock));
 
     fixture.detectChanges(); // ngOnInit
+
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('#should render the product', () => {
+    const productId = '2';
+    const productMock = {
+      ...generateOneProduct(),
+      id: productId,
+    };
+
+    activatedRouteStub.setParamMap({ id: productId });
+    productServiceSpy.getOne.and.returnValue(observableMock(productMock));
+
+    fixture.detectChanges();
+
+    const imgElement: HTMLImageElement = queryElementByTestId(
+      fixture,
+      'product-image',
+    ).nativeElement;
+    const h1TextContent = getTextContent(fixture, 'product-title');
+    const h2TextContent = getTextContent(fixture, 'product-price');
+    const pTextContent = getTextContent(fixture, 'product-description');
+
+    expect(imgElement.src).toBe(productMock.images[0]);
+    expect(h1TextContent).toBe(productMock.title);
+    expect(h2TextContent).toContain(productMock.price.toString());
+    expect(pTextContent).toBe(productMock.description);
+
+    expect(productServiceSpy.getOne).toHaveBeenCalledOnceWith(productId);
   });
 });
